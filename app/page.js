3d509ae1238bd6ff7650afd6a2b6bc3c0a703934
Yash Jawale <yash25.j@gmail.com>
2023-10-21 11:57:20 +0530

    Add markers

--- app/page.js
+++ app/page.js
@@ -6,8 +6,22 @@ export default function Home() {
     <main className='container mx-auto px-4 py-6'>
       <div className='grid gap-4 sm:grid-cols-2 md:grid-cols-3'>
         <Sandbox title="Hello" body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum delectus inventore aut, nulla deleniti rem odio ea laborum animi fugiat?" />
-        <Sandbox title="Hello" body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum delectus inventore aut, nulla deleniti rem odio ea laborum animi fugiat?" />
-        <Sandbox title="Hello" body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum delectus inventore aut, nulla deleniti rem odio ea laborum animi fugiat?" />
+
+        {/* Hero Section */}
+
+        {/* ... */}
+
+        {/* Who are we section */}
+
+        {/* ... */}
+
+        {/* Stats section */}
+
+        {/* ... */}
+
+        {/* Events carousel */}
+
+        {/* ... */}
       </div>
     </main>
   )
