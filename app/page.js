e7c99dd5c7430e2c1f42eac4797fb38c9d331769
Sarvesh Sandeep Rasal <justanothersarvesh@gmail.com>
2023-10-24 20:44:46 +0530

    Added Footer Component

--- app/page.js
+++ app/page.js
@@ -1,16 +1,15 @@
 import Image from 'next/image'
 import Sandbox from './components/Sandbox'
 import Event_Section from "./components/EventCard";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <main className='container mx-auto px-4 py-6'>
-      <div className='grid gap-4 sm:grid-cols-2 md:grid-cols-3'>
-        <Sandbox title="Hello" body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum delectus inventore aut, nulla deleniti rem odio ea laborum animi fugiat?" />
-        <Sandbox title="Hello" body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum delectus inventore aut, nulla deleniti rem odio ea laborum animi fugiat?" />
-        <Sandbox title="Hello" body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum delectus inventore aut, nulla deleniti rem odio ea laborum animi fugiat?" />
-      </div>
-        <Event_Section/>
+    <main>
+        <div className='flex justify-center px-4 w-[100%] mx-auto my-4'>
+            <Event_Section/>
+        </div>
+        <Footer/>
     </main>
   )
 }
