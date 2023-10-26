c358d9f903bddb2de3e1f11cca1d433ba4dc072a
Yash Jawale <yash25.j@gmail.com>
2023-10-26 08:02:57 +0530

    Combine sections

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 import Sandbox from './components/Sandbox'
 import Navbar from './components/Navbar'
-import StatsSection from './components/StatsSection.js';
+import StatsSection from './components/StatsSection';
 import Footer from "./components/Footer";
 import Hero from './components/Hero';
-import BlogCard from './components/BlogCard.js';
+import BlogCard from './components/BlogCard';
 import EventsSection from './components/EventsSection';
 import WhoAreWeSection from './components/WhoAreWeSection';
 
@@ -12,19 +12,11 @@ function App() {
   return (
     <>
       <Navbar />
-      {/* <Hero /> */}
-      {/* Who are we section */}
-
+      <Hero />
       <WhoAreWeSection/>
-
-      {/* Stats section */}
-      {/* <StatsSection/> */}
-      {/* <Home /> */}
-
-      {/* <Event_Section/> */}
-      {/* <EventsSection/> */}
-
-      {/* <Footer/> */}
+      <StatsSection/>
+      <EventsSection/>
+      <Footer/>
     </>
   )
 }
