ce1091ae11cb4bce06a286fb5ab857ba99469cc9
Yash Jawale <yash25.j@gmail.com>
2023-10-27 06:21:26 +0530

    Move event card to another component

--- app/components/EventCard.jsx
+++ app/components/EventCard.jsx
@@ -1,6 +1,14 @@
-function EventCard() {
+import Link from "next/link"
+
+function EventCard({name}) {
   return (
-    <div>EventCard</div>
+    <Link className="relative snap-start" href="/">
+      <div className="w-96 h-60 bg-slate-900 text-white flex items-end p-5 rounded-lg">
+        <h3 className="uppercase font-title font-medium text-4xl leading-tight">
+          {name}
+        </h3>
+      </div>
+    </Link>
   )
 }
 
