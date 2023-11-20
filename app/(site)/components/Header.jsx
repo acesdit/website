f41e9e13932d68fd7d4d01f14b7cd5b8e209cc35
Yash Jawale <yash25.j@gmail.com>
2023-11-20 20:34:43 +0530

    Refactor header

---
+++ app/(site)/components/Header.jsx
@@ -0,0 +1,15 @@
+function Header({title}) {
+  return (
+    <div className="hero-container nav-target">
+      <main className="hero-bg">
+        <div className="container mx-auto p-4 md:px-6">
+          <div className="flex items-center pt-20 pb-8 text-white">
+            <h1 className="font-title uppercase font-medium text-5xl">{title}</h1>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Header
