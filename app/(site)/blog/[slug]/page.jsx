bbe7fc8a71de2013091801218574d8bc7398406c
Yash Jawale <yash25.j@gmail.com>
2023-11-20 20:38:39 +0530

    Refactor breadcrumb

--- app/(site)/blog/[slug]/page.jsx
+++ app/(site)/blog/[slug]/page.jsx
@@ -7,6 +7,7 @@ import { DateTime } from 'luxon'
 import { urlForImage } from '@/sanity/lib/image'
 import { getImageDimensions } from '@sanity/asset-utils'
 import Link from 'next/link'
+import BreadCrumb from '../../components/BreadCrumb'
 
 const SampleImageComponent = ({ value }) => {
   const { width, height } = getImageDimensions(value)
@@ -42,18 +43,7 @@ async function BlogPost({ params }) {
 
   return (
     <>
-      <div className="hero-container nav-target">
-        <main className="hero-bg">
-          <div className="container mx-auto p-4 md:px-6">
-            <div className="flex items-center pt-20 pb-4 text-white">
-              <span className="text-lg">
-                <span className="uppercase font-title font-medium"><Link className="transition-colors hover:text-primary" href="/blog">Blog</Link> /</span>{' '}
-                {post.title}
-              </span>
-            </div>
-          </div>
-        </main>
-      </div>
+      <BreadCrumb parent="Blog" parentLink="/blog" child={post.title}/>
 
       <div className="container mx-auto p-4 md:px-6 pt-20">
         <h1 className="font-title font-medium uppercase text-5xl pb-4">
