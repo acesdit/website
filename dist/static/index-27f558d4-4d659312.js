93dff8eee95b98defe7f2ea1e5b41d9ba97d1492
Yash Jawale <yash25.j@gmail.com>
2023-11-23 23:21:57 +0530

    Remove build files

--- dist/static/index-27f558d4-4d659312.js
+++
@@ -1,1 +0,0 @@
-import{DocumentPane as t}from"./index-f6f16e84-42afd15f.js";import{D as m}from"./sanity-8d68a64d.js";import"./json-inspector-456562a0.js";export{m as DocumentPaneProvider,t as default};
