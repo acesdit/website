c8af7f2dc3f352cb85fa9d78a9663465e0ac8a1f
yashjawale <yash25.j@gmail.com>
2024-01-04 08:19:56 +0530

    Update hero text

--- app/(site)/components/Hero.jsx
+++ app/(site)/components/Hero.jsx
@@ -6,17 +6,15 @@ function Hero() {
     <div className="hero-container">
       <main className="hero-bg h-[50%] max-h-[94vh] md:max-h-[80vh] lg:max-h-[90vh] overflow-hidden pt-0 md:pt-24 nav-target">
         <div className="container mx-auto flex flex-col md:flex-row p-4 md:p-6">
-          <div className="md:w-5/12 flex flex-col py-20 text-white">
+          <div className="md:w-1/2 flex flex-col py-20 text-white">
             <h1 className="font-title uppercase font-semibold md:max-lg:text-5xl text-7xl pb-3 leading-[1.2]">
               Masters of Excellence
             </h1>
-            <p className="text-lg font-medium leading-relaxed">
-              Association of Computer Engineering Students is the departmental
-              club of Department of Computer Engineering at Dr. D. Y. Patil
-              Institute of Technology, Pimpri
+            <p className="text-lg font-medium leading-relaxed md:max-w-lg">
+              Empowering Students from over a decade, Association of Computer Engineering Students strives to create a supportive & collaborative community for all round development.
             </p>
           </div>
-          <div className="md:w-7/12">
+          <div className="md:w-1/2">
             <Image
               className="relative top-[-80px] sm:top-0"
               src={phoenix}
